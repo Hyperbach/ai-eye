38c612e9155910ac83f08c93fb7855a8def28252
Sergey A <varnie29a@mail.ru>
2023-02-03 17:50:26 +0500

    code cleanup in models

--- apps/core/models.py
+++ apps/core/models.py
@@ -39,7 +39,7 @@ class CustomUserManager(BaseUserManager):
         if not email:
             raise ValueError("Users require an email field")
         email = self.normalize_email(email)
-        user: User = self.model(email=email, **extra_fields)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
