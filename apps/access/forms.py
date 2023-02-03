1d94e15c2ebc39690fa452d9d39a523eeadc054c
Sergey A <varnie29a@mail.ru>
2023-02-03 17:07:32 +0500

    improve LoginForm

--- apps/access/forms.py
+++ apps/access/forms.py
@@ -1,15 +1,13 @@
 from django.contrib.auth.forms import AuthenticationForm
 from django.core.exceptions import ValidationError
 
-from core.models import User
-
 
 class LoginForm(AuthenticationForm):
     def clean(self):
         cleaned_data = super().clean()
 
-        user: User = self.get_user()
-        if not user.is_aieye_admin:
+        user = self.get_user()
+        if user is None or not user.is_aieye_admin:
             raise ValidationError(
                 "Insufficient privileges",
                 code="wrong_role",
