ccde6208a5e086a2742fae8a7b8b0ceeec5ad873
Sergey A <varnie29a@mail.ru>
2023-02-03 17:09:55 +0500

    add custom Token Authentication

--- apps/api/authentication.py
+++ apps/api/authentication.py
@@ -1,6 +1,7 @@
 from django.utils.translation import gettext_lazy as _
 
 import rest_framework.authentication
+from core.enums import UserGroupType
 from core.models import PublicToken
 from rest_framework import exceptions
 
@@ -12,7 +13,16 @@ class AiEyeTokenAuthentication(rest_framework.authentication.TokenAuthentication
     def authenticate_credentials(self, key):
         user, token = super().authenticate_credentials(key)
 
+        # token is "Public token"
         if not token.is_active:
             raise exceptions.AuthenticationFailed(_("Inactive token."))
+
+        if not token.openaikey.is_active:
+            raise exceptions.AuthenticationFailed(_("Inactive related OpenAI token."))
+
+        if not user.is_aieye_user:
+            raise exceptions.AuthenticationFailed(
+                _(f"Invalid user role, should be {UserGroupType.AIEYE_USERS}.")
+            )
 
         return user, token
