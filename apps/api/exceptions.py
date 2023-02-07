29e50a6c9cf92ee30c8330fea282f20edcb59b18
Sergey A <varnie29a@mail.ru>
2023-02-07 17:12:21 +0500

    add service

---
+++ apps/api/exceptions.py
@@ -0,0 +1,10 @@
+from django.utils.translation import gettext_lazy as _
+
+from rest_framework import status
+from rest_framework.exceptions import APIException
+
+
+class OpenAIRequestException(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = _("Invalid OpenAI request.")  # type: ignore
+    default_code = "error"
