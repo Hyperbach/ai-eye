545077cdce7e75579a4d6256b36860571b65ada4
Sergey A <varnie29a@mail.ru>
2023-02-07 14:03:54 +0500

    update Log table

--- apps/api/models.py
+++ apps/api/models.py
@@ -16,17 +16,15 @@ def get_expire_time():
 class Log(models.Model):
     api_type = models.CharField(max_length=50)
     endpoint = models.CharField(max_length=100)
-    method = models.CharField(max_length=10)
     parameters = models.TextField(db_index=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     api_key = models.ForeignKey(OpenAIKey, on_delete=models.CASCADE)
     response = models.TextField()
     cache_hit = models.BooleanField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    expire_time = models.DateTimeField(default=get_expire_time)
 
     def __str__(self):
-        return f"{self.api_type} {self.endpoint} {self.method} {self.parameters}"
+        return f"{self.api_type} {self.endpoint} {self.parameters}"
 
     class Meta:
         verbose_name = _("Log")
