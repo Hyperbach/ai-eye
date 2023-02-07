e86bd9c21ba30ea26fa03dce60f4f91f84da73a4
Sergey A <varnie29a@mail.ru>
2023-02-07 23:46:47 +0500

    remove index

--- apps/api/models.py
+++ apps/api/models.py
@@ -12,12 +12,19 @@ User = get_user_model()
 class Log(models.Model):
     api_type = models.CharField(max_length=50)
     endpoint = models.CharField(max_length=100)
-    parameters = models.TextField(db_index=True)
+    parameters = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     api_key = models.ForeignKey(OpenAIKey, on_delete=models.CASCADE)
     response = models.TextField()
     cache_hit = models.BooleanField()
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = _("Log")
+        verbose_name_plural = _("Logs")
+
+    def __str__(self):
+        return f"{self.api_type} {self.endpoint} {self.parameters}"
 
     @staticmethod
     def stringify_parameters(parameters: dict[str, Any]):
@@ -25,10 +32,3 @@ class Log(models.Model):
         parameters_keys.sort()
 
         return "&".join([f"{k}={parameters[k]}" for k in parameters_keys])
-
-    def __str__(self):
-        return f"{self.api_type} {self.endpoint} {self.parameters}"
-
-    class Meta:
-        verbose_name = _("Log")
-        verbose_name_plural = _("Logs")
