12fefc2ba7227df65d13eb30693cc643cd97ca0c
Sergey A <varnie29a@mail.ru>
2023-02-07 17:12:50 +0500

    add service

---
+++ apps/api/services.py
@@ -0,0 +1,27 @@
+from typing import Any
+
+import requests
+from api.exceptions import OpenAIRequestException
+
+OPENAI_HOST = "https://api.openai.com"
+
+
+def openai_request(openaikey: str, endpoint: str, parameters: dict[str, Any]):
+    openai_api_url = f"{OPENAI_HOST}/{endpoint}"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {openaikey}",
+    }
+
+    try:
+        response = requests.post(url=openai_api_url, headers=headers, json=parameters)
+        response.raise_for_status()
+    except requests.RequestException as exc:
+        raise OpenAIRequestException(detail=str(exc))
+    else:
+        response_json = response.json()
+
+        try:
+            return response_json["choices"][0]["text"].strip()
+        except (KeyError, ValueError) as exc:
+            raise OpenAIRequestException(detail=str(exc))
