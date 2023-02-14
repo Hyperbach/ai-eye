cd6113973553f800609a107af1bfa77a89fb1f2e
Sergey A <varnie29a@mail.ru>
2023-02-14 11:28:06 +0500

    update mixins

--- apps/api/mixins.py
+++ apps/api/mixins.py
@@ -10,20 +10,15 @@ class PrepareParametersMixin:
         "v1/edits",
     ]
 
-    def prepare_parameters(self):
+    def prepare_parameters(self, data):
         endpoint = self.kwargs["endpoint"]
         if endpoint not in self.ALLOWED_OPENAI_ENDPOINTS:
             raise ValidationError("Invalid data")
 
-        if not self.request.data:
-            raise ValidationError("Invalid data")
-        if (
-            "api_type" not in self.request.data
-            or type(self.request.data["api_type"]) is not str
-        ):
+        if not data:
             raise ValidationError("Invalid data")
 
-        parameters = {k: v for k, v in self.request.data.items() if k != "api_type"}
+        parameters = data
         parameters_stringified = Log.stringify_parameters(parameters)
 
         return parameters, parameters_stringified
