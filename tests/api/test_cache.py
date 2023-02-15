2dc2be245f03d8fcdf5d9b47111deb3833d9e605
Sergey A <varnie29a@mail.ru>
2023-02-15 20:50:56 +0500

    add comments, cleanup

--- tests/api/test_cache.py
+++ tests/api/test_cache.py
@@ -17,39 +17,35 @@ from tests.factories import (
 
 
 def openai_request_mock(openaikey: str, endpoint: str, parameters: Dict[str, Any]):
-    # actually we don't care in tests what response it returns because we test data caching only
+    # actually we don't care what response it returns because we test data caching only
 
     params_glued = "_".join([f"{k}_{v}" for k, v in parameters.items()])
     return f"{openaikey} {endpoint} {params_glued}"
 
 
 class CacheTests(TestCase):
-    @classmethod
-    def setUpTestData(cls):
+    def setUp(self) -> None:
         aieye_admin = AiEyeAdminFactory.create()
         aieye_user = AiEyeUserFactory.create()
         openaikey = OpenAIKeyFactory.create(owner=aieye_admin)
 
-        cls.aieye_admin = aieye_admin
-        cls.aieye_user = aieye_user
-        cls.openaikey = openaikey
-        cls.publictoken = PublicTokenFactory.create(
+        self.aieye_admin = aieye_admin
+        self.aieye_user = aieye_user
+        self.openaikey = openaikey
+        self.publictoken = PublicTokenFactory.create(
             user=aieye_user, openaikey=openaikey
         )
 
-    def setUp(self) -> None:
         self.client = APIClient()
+        public_token = self.publictoken.key
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + public_token)
 
     def invoke_openai_api(self, request_data, endpoint):
-        public_token = self.publictoken.key
         openai_api_url = reverse("api:openai-list", kwargs={"endpoint": endpoint})
-        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + public_token)
         return self.client.post(openai_api_url, data=request_data)
 
     def invoke_cache_api(self, request_data, endpoint):
-        public_token = self.publictoken.key
         cache_api_url = reverse("api:cache-list", kwargs={"endpoint": endpoint})
-        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + public_token)
         return self.client.get(cache_api_url, data=request_data)
 
     def exec(self, request_data, endpoint):
@@ -60,25 +56,30 @@ class CacheTests(TestCase):
         )
 
         caches_count_before = Log.objects.filter(**filter_kwargs).count()
+        # there should be no cache before the request is performed
         self.assertEqual(caches_count_before, 0)
 
         response = self.invoke_openai_api(request_data, endpoint)
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
         caches_count_after = Log.objects.filter(**filter_kwargs).count()
+        # there should be 1 cache record after the request is performed
         self.assertEqual(caches_count_after, 1)
 
-        cache_hit_true_counts = Log.objects.filter(
+        cache_hit_counts = Log.objects.filter(
             **filter_kwargs | {"cache_hit": True}
         ).count()
-        cache_hit_false_counts = Log.objects.filter(
+        cache_miss_counts = Log.objects.filter(
             **filter_kwargs | {"cache_hit": False}
         ).count()
-        self.assertEqual(cache_hit_true_counts, 0)
-        self.assertEqual(cache_hit_false_counts, 1)
+        # there should be 0 cache hit records because the request is performed for the first time
+        self.assertEqual(cache_hit_counts, 0)
+        # there should be 1 cache miss records because the request is performed for the first time
+        self.assertEqual(cache_miss_counts, 1)
 
         response = self.invoke_cache_api(request_data, endpoint)
         self.assertEqual(response.status_code, HTTPStatus.OK)
+        # there should be 0 cache hits records in the API response
         self.assertEqual(len(response.data), 0)
 
         # perform a consequent request
@@ -86,19 +87,23 @@ class CacheTests(TestCase):
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
         caches_count_after = Log.objects.filter(**filter_kwargs).count()
+        # there should be 2 cache records in total after the consequent request is performed
         self.assertEqual(caches_count_after, 2)
 
-        cache_hit_true_counts = Log.objects.filter(
+        cache_hit_counts = Log.objects.filter(
             **filter_kwargs | {"cache_hit": True}
         ).count()
-        cache_hit_false_counts = Log.objects.filter(
+        cache_miss_counts = Log.objects.filter(
             **filter_kwargs | {"cache_hit": False}
         ).count()
-        self.assertEqual(cache_hit_true_counts, 1)
-        self.assertEqual(cache_hit_false_counts, 1)
+        # there should be 1 cache hit records because the same request is performed for the second time
+        self.assertEqual(cache_hit_counts, 1)
+        # there should be 1 cache miss records, i.e. the very first request
+        self.assertEqual(cache_miss_counts, 1)
 
         response = self.invoke_cache_api(request_data, endpoint)
         self.assertEqual(response.status_code, HTTPStatus.OK)
+        # there should be 1 cache hits records in the API response now
         self.assertEqual(len(response.data), 1)
         self.assertEqual(response.data[0]["cache_hit"], True)
 
