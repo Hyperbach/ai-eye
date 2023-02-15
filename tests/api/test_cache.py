52028dcf82fd233967dc01729434a45284f64902
Sergey A <varnie29a@mail.ru>
2023-02-15 20:40:24 +0500

    add tests for API

---
+++ tests/api/test_cache.py
@@ -0,0 +1,121 @@
+from http import HTTPStatus
+from typing import Any, Dict
+from unittest.mock import patch
+
+from django.test import TestCase
+from django.urls import reverse
+
+from api.models import Log
+from rest_framework.test import APIClient
+
+from tests.factories import (
+    AiEyeAdminFactory,
+    AiEyeUserFactory,
+    OpenAIKeyFactory,
+    PublicTokenFactory,
+)
+
+
+def openai_request_mock(openaikey: str, endpoint: str, parameters: Dict[str, Any]):
+    # actually we don't care in tests what response it returns because we test data caching only
+
+    params_glued = "_".join([f"{k}_{v}" for k, v in parameters.items()])
+    return f"{openaikey} {endpoint} {params_glued}"
+
+
+class CacheTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        aieye_admin = AiEyeAdminFactory.create()
+        aieye_user = AiEyeUserFactory.create()
+        openaikey = OpenAIKeyFactory.create(owner=aieye_admin)
+
+        cls.aieye_admin = aieye_admin
+        cls.aieye_user = aieye_user
+        cls.openaikey = openaikey
+        cls.publictoken = PublicTokenFactory.create(
+            user=aieye_user, openaikey=openaikey
+        )
+
+    def setUp(self) -> None:
+        self.client = APIClient()
+
+    def invoke_openai_api(self, request_data, endpoint):
+        public_token = self.publictoken.key
+        openai_api_url = reverse("api:openai-list", kwargs={"endpoint": endpoint})
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + public_token)
+        return self.client.post(openai_api_url, data=request_data)
+
+    def invoke_cache_api(self, request_data, endpoint):
+        public_token = self.publictoken.key
+        cache_api_url = reverse("api:cache-list", kwargs={"endpoint": endpoint})
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + public_token)
+        return self.client.get(cache_api_url, data=request_data)
+
+    def exec(self, request_data, endpoint):
+        filter_kwargs = dict(
+            endpoint=endpoint,
+            parameters=Log.stringify_parameters(request_data),
+            user=self.aieye_user,
+        )
+
+        caches_count_before = Log.objects.filter(**filter_kwargs).count()
+        self.assertEqual(caches_count_before, 0)
+
+        response = self.invoke_openai_api(request_data, endpoint)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+        caches_count_after = Log.objects.filter(**filter_kwargs).count()
+        self.assertEqual(caches_count_after, 1)
+
+        cache_hit_true_counts = Log.objects.filter(
+            **filter_kwargs | {"cache_hit": True}
+        ).count()
+        cache_hit_false_counts = Log.objects.filter(
+            **filter_kwargs | {"cache_hit": False}
+        ).count()
+        self.assertEqual(cache_hit_true_counts, 0)
+        self.assertEqual(cache_hit_false_counts, 1)
+
+        response = self.invoke_cache_api(request_data, endpoint)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(len(response.data), 0)
+
+        # perform a consequent request
+        response = self.invoke_openai_api(request_data, endpoint)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+        caches_count_after = Log.objects.filter(**filter_kwargs).count()
+        self.assertEqual(caches_count_after, 2)
+
+        cache_hit_true_counts = Log.objects.filter(
+            **filter_kwargs | {"cache_hit": True}
+        ).count()
+        cache_hit_false_counts = Log.objects.filter(
+            **filter_kwargs | {"cache_hit": False}
+        ).count()
+        self.assertEqual(cache_hit_true_counts, 1)
+        self.assertEqual(cache_hit_false_counts, 1)
+
+        response = self.invoke_cache_api(request_data, endpoint)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]["cache_hit"], True)
+
+    @patch("api.views.openai_request", side_effect=openai_request_mock)
+    def test_cache_case1(self, mock_openai_request):
+        request_data = {"model": "text-davinci-003", "prompt": "how do you do"}
+        endpoint = "v1/completions"
+        self.exec(request_data, endpoint)
+
+    @patch("api.views.openai_request", side_effect=openai_request_mock)
+    def test_cache_case_2(self, mock_openai_request):
+        request_data = {"model": "text-davinci-003", "prompt": ""}
+        endpoint = "v1/completions"
+        self.exec(request_data, endpoint)
+
+    @patch("api.views.openai_request", side_effect=openai_request_mock)
+    def test_cache_case_3(self, mock_openai_request):
+        request_data = {"model": "text-davinci-003", "prompt": ""}
+        endpoint = "v1/completions"
+        self.exec(request_data, endpoint)
