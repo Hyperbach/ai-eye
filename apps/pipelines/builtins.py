3dccd884cf06c36ed760205362c6dab256f72f52
Sergey A <varnie29a@mail.ru>
2023-03-02 12:52:52 +0500

    type hinting

--- apps/pipelines/builtins.py
+++ apps/pipelines/builtins.py
@@ -1,23 +1,24 @@
 import datetime
 import inspect
 import sys
+from typing import Any, Callable, Dict
 
 import pytz
 
 
 # example of builtin function
-def concat(arg1, arg2):
+def concat(arg1, arg2) -> str:
     return f"{arg1} {arg2}"
 
 
 # example of builtin function
-def now(timezone):
+def now(timezone) -> str:
     tz = pytz.timezone(timezone)
     return str(datetime.datetime.now(tz=tz))
 
 
 # example of builtin function
-def identity(s):
+def identity(s: str) -> str:
     return s
 
 
@@ -42,7 +43,7 @@ def get_builtin_function_names():
     )
 
 
-def call_builtin_function(name, **kwargs):
+def call_builtin_function(name, **kwargs) -> Any:
     return BUILTIN_FUNCTIONS_DICT[name](**kwargs)
 
 
@@ -66,4 +67,4 @@ IGNORED_FUNCTION_NAMES = [
     "call_builtin_function",
 ]
 
-BUILTIN_FUNCTIONS_DICT = dict_functions(sys.modules[__name__])
+BUILTIN_FUNCTIONS_DICT: Dict[str, Callable] = dict_functions(sys.modules[__name__])
