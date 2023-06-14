86949a9d21279eb78c43bcd6cc345df25a593aac
Khvatov Dmitry <dimax4@gmail.com>
2023-06-15 00:30:48 +0200

    Add type_inference_decorator, add idempotency test

--- funcs/builtins.py
+++ funcs/builtins.py
@@ -1,112 +1,104 @@
+import datetime
 import functools
 import inspect
 import json
 from datetime import datetime
 
 
-def exception_handler(func):
+def type_inference_decorator(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        try:
-            value = func(*args, **kwargs)
-            return output_wrapper(value=value)
-        except Exception as e:
-            return output_wrapper(error=str(e))
+        # Attempt to infer the type of the input arguments
+        new_args = []
+        for arg in args:
+            try:
+                # Try to interpret argument as JSON
+                new_arg = json.loads(arg)
+            except json.JSONDecodeError:
+                try:
+                    # Try to interpret argument as a float
+                    new_arg = float(arg)
+                except ValueError:
+                    # If all else fails, assume it's a plain string
+                    new_arg = arg
+            new_args.append(new_arg)
+
+        # Call the function
+        result = func(*new_args, **kwargs)
+
+        # Check if result is a dict or list (i.e., JSON-serializable), if so, convert it to a JSON string
+        if isinstance(result, (list, dict)):
+            return json.dumps(result)
+        else:
+            return str(result)
 
     wrapper.__signature__ = inspect.signature(func)
 
     return wrapper
 
 
-# Wrapper function
-def output_wrapper(value=None, error=None):
-    output = {}
-    if value is not None:
-        output["result"] = value
-    if error is not None:
-        output["error"] = error
-    return json.dumps(output)
-
-
 # Math functions
-@exception_handler
+@type_inference_decorator
 def add(a, b):
-    a = float(a)
-    b = float(b)
     return a + b
 
 
-@exception_handler
+@type_inference_decorator
 def subtract(a, b):
-    a = float(a)
-    b = float(b)
     return a - b
 
 
-@exception_handler
+@type_inference_decorator
 def multiply(a, b):
-    a = float(a)
-    b = float(b)
     return a * b
 
 
-@exception_handler
+@type_inference_decorator
 def divide(a, b):
-    a = float(a)
-    b = float(b)
-    if b != 0:
-        return a / b
-    else:
-        raise ValueError("Division by zero")
+    return a / b
 
 
 # String functions
-@exception_handler
+@type_inference_decorator
 def length(s):
     return len(s)
 
 
-@exception_handler
+@type_inference_decorator
 def uppercase(s):
     return s.upper()
 
 
-@exception_handler
+@type_inference_decorator
 def lowercase(s):
     return s.lower()
 
 
-@exception_handler
+@type_inference_decorator
 def concat(s1, s2):
     return s1 + s2
 
 
 # Data manipulation
-@exception_handler
+@type_inference_decorator
 def append(lst, el):
-    lst = json.loads(lst)
-    el = json.loads(el)
     lst.append(el)
     return lst
 
 
-@exception_handler
+@type_inference_decorator
 def remove(lst, el):
-    lst = json.loads(lst)
-    el = json.loads(el)
-    if el in lst:
-        lst.remove(el)
+    lst.remove(el)
     return lst
 
 
-@exception_handler
+@type_inference_decorator
 def sort(lst):
-    lst = json.loads(lst)
     lst.sort()
     return lst
 
 
-@exception_handler
+@type_inference_decorator
 def now():
     return datetime.now().isoformat()
 
--- tests/pipelines/test_builtins.py
+++ tests/pipelines/test_builtins.py
@@ -1,40 +1,54 @@
-import json
 from django.test import TestCase
-from funcs.builtins import add, subtract, multiply, divide, length, append, remove, sort, uppercase, lowercase, concat, now
+from funcs.builtins import add, subtract, multiply, divide, length, append, remove, sort, uppercase, lowercase, concat, \
+    now
 from datetime import datetime, timedelta
+import json
 
 
 class BuiltinFunctionTests(TestCase):
 
     def test_functions(self):
         # Test math functions
-        self.assertEqual(add("1", "2"), '{"result": 3.0}')
-        self.assertEqual(subtract("4", "2"), '{"result": 2.0}')
-        self.assertEqual(multiply("3", "3"), '{"result": 9.0}')
-        self.assertEqual(divide("10", "2"), '{"result": 5.0}')
-        self.assertEqual(divide("10", "0"), '{"error": "Division by zero"}')
+        self.assertEqual(add("1", "2"), '3')
+        self.assertEqual(subtract("4", "2"), '2')
+        self.assertEqual(multiply("3", "3"), '9')
+        self.assertEqual(divide("10", "2"), '5.0')
+
+        with self.assertRaises(Exception):
+            divide("10", "0")
+
+        # Additional test for adding integer and float strings
+        self.assertEqual(add("3", "2.0"), '5.0')
+        self.assertEqual(add("2.0", "3"), '5.0')
 
         # Test string functions
-        self.assertEqual(length("hello"), '{"result": 5}')
-        self.assertEqual(uppercase("hello"), '{"result": "HELLO"}')
-        self.assertEqual(lowercase("HELLO"), '{"result": "hello"}')
-        self.assertEqual(concat("hello", " world"), '{"result": "hello world"}')
+        self.assertEqual(length("hello"), '5')
+        self.assertEqual(uppercase("hello"), 'HELLO')
+        self.assertEqual(lowercase("HELLO"), 'hello')
+        self.assertEqual(concat("hello", " world"), 'hello world')
 
         # Test list functions
-        self.assertEqual(append('["a", "b", "c"]', '"d"'), '{"result": ["a", "b", "c", "d"]}')
-        self.assertEqual(remove('["a", "b", "c"]', '"b"'), '{"result": ["a", "c"]}')
-        self.assertEqual(sort('["c", "b", "a"]'), '{"result": ["a", "b", "c"]}')
+        self.assertEqual(append('["a", "b", "c"]', '"d"'), '["a", "b", "c", "d"]')
+        self.assertEqual(remove('["a", "b", "c"]', '"b"'), '["a", "c"]')
+        self.assertEqual(sort('["c", "b", "a"]'), '["a", "b", "c"]')
 
     def test_now_function(self):
         now_result = now()
-        now_json = json.loads(now_result)
-
-        # Check if result key exists in the response
-        self.assertIn("result", now_json)
 
         # Parse datetime from the result
-        result_datetime = datetime.fromisoformat(now_json["result"])
+        result_datetime = datetime.fromisoformat(now_result)
 
         # Check if the result datetime is close to the current datetime
         # (account for possible small delays between function execution and test)
         self.assertTrue(datetime.now() - result_datetime < timedelta(seconds=1))
+
+    def test_idempotency_and_chainability(self):
+        x = '["c", "b", "a"]'
+        y = '2'
+        z = '3'
+
+        # Check idempotency for sort
+        self.assertEqual(sort(x), sort(sort(x)))
+
+        # Check chainability for add
+        self.assertEqual(add(y, add(y, z)), '7')
