f8f1f6cdfbccd2078621ba263b7942eb063ff964
Khvatov Dmitry <dimax4@gmail.com>
2023-11-27 14:08:46 +0100

    Improve generate-db lil bit

--- generate_kb.py
+++ generate_kb.py
@@ -1,70 +1,8 @@
 import os
+import subprocess
 
-
-# Function to check if a file is binary by examining its content
-def is_binary(file_path):
-    try:
-        with open(file_path, 'rb') as f:
-            # Read the first 8000 bytes and check for null bytes
-            return b'\x00' in f.read(8000)
-    except Exception as e:
-        # Handle any exceptions that may occur during file reading and log them
-        print(f"Error while checking binary for {file_path}: {str(e)}")
-        return True
-
-
-def pack_project_to_kb(source_dir, output_file, dirs_list):
-    added_files = []  # List to store added file paths
-    with open(output_file, 'w') as outfile:
-        for dir_name in dirs_list:
-            dir_path = os.path.join(source_dir, dir_name)
-            # Check if the directory exists
-            if os.path.exists(dir_path) and os.path.isdir(dir_path):
-                # Writing the directory path
-                outfile.write(dir_path + '\n')
-                print(f"Directory path: {dir_path}")
-                for file_name in os.listdir(dir_path):
-                    file_path = os.path.join(dir_path, file_name)
-                    if os.path.isfile(file_path):
-                        print(f"Checking file: {file_path}")
-                        if not is_binary(file_path):
-                            # Check if the file is not binary
-                            # Excluding specific files
-                            if file_path not in ['./client/package-lock.json', './client/yarn.lock']:
-                                # Writing the file path
-                                outfile.write(file_path + '\n')
-                                added_files.append(file_path)  # Add file to the list
-                                print(f"Writing file: {file_path}")
-                                # Reading and writing the file content
-                                with open(file_path, 'r', encoding='utf-8', errors='ignore') as infile:
-                                    outfile.write(infile.read())
-                                # Writing the separator line
-                                outfile.write('\n' + '-' * 55 + '\n')
-                                print(f"Separator line written")
-                            else:
-                                print(f"Excluded file: {file_path}")
-                        else:
-                            print(f"File is binary: {file_path}")
-            else:
-                print(f"Directory not found: {dir_path}")
-
-    # Calculate statistics
-    num_files_added = len(added_files)
-    total_size_kb = sum(os.path.getsize(file) for file in added_files) / 1024
-    print(f"Number of files added: {num_files_added}")
-    print(f"Total size of resulting file (KB): {total_size_kb:.2f}")
-
-    # Get the top 20 files by size in descending order
-    top_20_files = sorted(added_files, key=lambda file: os.path.getsize(file), reverse=True)[:20]
-    print("Top 20 files by size (in kilobytes):")
-    for file_path in top_20_files:
-        file_size_kb = os.path.getsize(file_path) / 1024
-        print(f"{file_path} - {file_size_kb:.2f} KB")
-
-
-# Specify the source directory, output file, and list of directories to pack
-source_directory = './'
 output_kb_file = 'aieye.kb'
+excluded_files = ['./client/package-lock.json', './client/yarn.lock']
 directories_to_pack = [
     'configs',
     'templates',
@@ -99,5 +37,103 @@ directories_to_pack = [
     'apps/dashboard/views',
     'apps/api'
 ]
+
+
+# Function to check if a file is binary by examining its content
+def is_binary(file_path):
+    try:
+        with open(file_path, 'rb') as f:
+            # Read the first 8000 bytes and check for null bytes
+            return b'\x00' in f.read(8000)
+    except Exception as e:
+        # Handle any exceptions that may occur during file reading and log them
+        print(f"Error while checking binary for {file_path}: {str(e)}")
+        return True
+
+
+# Function to check if a file is tracked by Git
+def is_git_tracked(file_path):
+    try:
+        # Convert file path to relative path from source_dir
+        relative_file_path = os.path.relpath(file_path, './')
+        # Running 'git ls-files' command to check if file is tracked by Git
+        result = subprocess.run(['git', 'ls-files', relative_file_path], capture_output=True, text=True, cwd='./')
+        return relative_file_path == result.stdout.strip()
+    except subprocess.SubprocessError as e:
+        print(f"Error checking Git for {file_path}: {str(e)}")
+        return False
+
+
+def pack_project_to_kb(source_dir, output_file, dirs_list):
+    added_files = []  # List to store added file paths
+    extension_stats = {}  # Dictionary to store file extension statistics
+
+    with open(output_file, 'w') as outfile:
+        for dir_name in dirs_list:
+            dir_path = os.path.join(source_dir, dir_name)
+            # Check if the directory exists
+            if os.path.exists(dir_path) and os.path.isdir(dir_path):
+                # Writing the directory path
+                outfile.write(dir_path + '\n')
+                print(f"Directory path: {dir_path}")
+                for file_name in os.listdir(dir_path):
+                    file_path = os.path.join(dir_path, file_name)
+                    if os.path.isfile(file_path):
+                        print(f"Checking file: {file_path}")
+                        # Excluding specific files
+                        if file_path not in excluded_files:
+                            if is_git_tracked(file_path):
+                                if not is_binary(file_path):
+                                    outfile.write(file_path + '\n')
+                                    added_files.append(file_path)  # Add file to the list
+                                    print(f"Writing file: {file_path}")
+                                    # Update extension statistics
+                                    ext = os.path.splitext(file_path)[1]
+                                    extension_stats[ext] = extension_stats.get(ext, 0) + 1
+                                    # Reading and writing the file content
+                                    with open(file_path, 'r', encoding='utf-8', errors='ignore') as infile:
+                                        outfile.write(infile.read())
+                                    # Writing the separator line
+                                    outfile.write('\n' + '-' * 55 + '\n')
+                                    print(f"Separator line written")
+                                else:
+                                    print(f"File is binary: {file_path}")
+                            else:
+                                print(f"Excluded non-Git file: {file_path}")
+                        else:
+                            print(f"Excluded file: {file_path}")
+                    else:
+                        print(f"Directory not found: {dir_path}")
+
+    def separator():
+        print('\n' + '-' * 55 + '\n')
+
+    separator()
+
+    # Calculate statistics
+    num_files_added = len(added_files)
+    total_size_kb = sum(os.path.getsize(file) for file in added_files) / 1024
+    print(f"Number of files added: {num_files_added}")
+    print(f"Total size of resulting file (KB): {total_size_kb:.2f}")
+
+    separator()
+
+    # Print extension statistics
+    print("File extension statistics:")
+    for ext, count in extension_stats.items():
+        print(f"{ext if ext else '(no extension)'}: {count} file(s)")
+
+    separator()
+
+    # Get the top 20 files by size in descending order
+    top_20_files = sorted(added_files, key=lambda file: os.path.getsize(file), reverse=True)[:20]
+    print("Top 20 files by size (in kilobytes):")
+    for file_path in top_20_files:
+        file_size_kb = os.path.getsize(file_path) / 1024
+        print(f"{file_path} - {file_size_kb:.2f} KB")
+
+
+# Specify the source directory, output file, and list of directories to pack
+source_directory = './'
 
 pack_project_to_kb(source_directory, output_kb_file, directories_to_pack)
