6f356039afe56d316be930f4ee14ed6d993b7577
Khvatov Dmitry <dimax4@gmail.com>
2023-11-28 00:39:18 +0100

    Fix file size calc

--- generate_kb.py
+++ generate_kb.py
@@ -113,7 +113,8 @@ def pack_project_to_kb(source_dir, output_file, dirs_list):
     num_files_added = len(added_files)
     total_size_kb = sum(os.path.getsize(file) for file in added_files) / 1024
     print(f"Number of files added: {num_files_added}")
-    print(f"Total size of resulting file (KB): {total_size_kb:.2f}")
+    file_size_kb = os.path.getsize(output_file) / 1024
+    print(f"Total size of resulting file (KB): {file_size_kb:.2f}")
 
     separator()
 
