1c7fed14f9a66d596e7064a09da7b263fef15d5c
Khvatov Dmitry <dimax4@gmail.com>
2024-02-27 14:13:34 +0100

    Update model names

--- apps/pipelines/choices.py
+++ apps/pipelines/choices.py
@@ -25,10 +25,12 @@ class AIServices(models.TextChoices):
 
 class TypesOfModels(models.TextChoices):
     # OpenAI Models
+    GPT_4_TURBO_PREVIEW = "gpt-4-turbo-preview", _("GPT-4 Turbo Preview")
     GPT_4_TURBO_1106 = "gpt-4-1106-preview", _("GPT-4 Turbo 1106")
     GPT_4_1106_VISION_PREVIEW = "gpt-4-1106-vision-preview", _("GPT-4 1106 Vision Preview")
     GPT_4 = "gpt-4", _("GPT-4")
     GPT_4_32K = "gpt-4-32k", _("GPT-4 32K")
+    GPT_35_TURBO = "gpt-3.5-turbo", _("GPT-3.5 Turbo")
     GPT_35_TURBO_1106 = "gpt-3.5-turbo-1106", _("GPT-3.5 Turbo 1106")
     GPT_35_TURBO_INSTRUCT = "gpt-3.5-turbo-instruct", _("GPT-3.5 Turbo Instruct")
 
@@ -42,10 +44,12 @@ class TypesOfModels(models.TextChoices):
 
     def get_pricing_details(self) -> Tuple[float, float]:
         pricing: dict[str, Tuple[float, float]] = {
+            "gpt-4-turbo-preview": (0.01, 0.03),
             "gpt-4-1106-preview": (0.01, 0.03),
             "gpt-4-1106-vision-preview": (0.01, 0.03),
             "gpt-4": (0.03, 0.06),
             "gpt-4-32k": (0.06, 0.12),
+            "gpt-3.5-turbo": (0.0005, 0.0015),
             "gpt-3.5-turbo-1106": (0.0010, 0.0020),
             "gpt-3.5-turbo-instruct": (0.0015, 0.0020),
             "mistralai/Mistral-7B-Instruct-v0.2": (0.0002, 0.0002),
